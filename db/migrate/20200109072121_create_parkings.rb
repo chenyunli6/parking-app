dc73aae40ac8f8929a3c8816b352396d36127736
chenyunli <m18301662790@gmail.com>
2020-01-09 16:39:50 +0800

    unit test

---
+++ db/migrate/20200109072121_create_parkings.rb
@@ -0,0 +1,13 @@
+class CreateParkings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :parkings do |t|
+      t.string :parking_type  # 费率类型: guest, short-term, long-term
+      t.datetime :start_at    # 开始时间
+      t.datetime :end_at      # 结束时间
+      t.integer :amount
+      t.integer :user_id, :index => true
+
+      t.timestamps
+    end
+  end
+end
